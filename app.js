5c07d0b85b440ba6dcd3b04bbe8b56c9132d77df
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-01-29 15:24:57 +1100

    connect cosmos db

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,8 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/admin', adminRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-const title= process.env.TITLE
+var {MongoClient} = require('mongodb');
+const appInsights = require('applicationinsights');
+
+const title = process.env.TITLE;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title });
+  const URL = process.env.URL;
+  const dbName = 'Library';
+  (async function mongo(){
+    let client;
+    try{
+      client = await MongoClient.connect(URL);
+      const db=client.db(dbName);
+      const response = await db.collection('books').find().toArray();
+      appInsights.defaultClient.trackEvent({name: 'mongoRequest', properties: {dbName, size: response.length}});
+      res.render('index', {books: response});
+    }catch(err){
+      console.log(err);
+    }
+  }());
 });
 
 module.exports = router;
